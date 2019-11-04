354bf63efc6bcfd1f900e31edca1d31e29f8e372
step-off <nofantasy12@gmail.com>
2019-11-04 14:36:56 +0500

    Add attributes to models

--- Domain.Models/Vehicle/VehicleMake.cs
+++ Domain.Models/Vehicle/VehicleMake.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace vega.Domain.Models
 {
@@ -9,6 +10,8 @@ namespace vega.Domain.Models
             Models = new List<VehicleModel>();
         }
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public List<VehicleModel> Models { get; set; }
     }
--- Domain.Models/Vehicle/VehicleModel.cs
+++ Domain.Models/Vehicle/VehicleModel.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace vega.Domain.Models
 {
+    [Table("Models")]
     public class VehicleModel
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
         public VehicleMake Make { get; set; }
         public int MakeId { get; set; }
