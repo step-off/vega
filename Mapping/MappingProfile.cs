4886cd69a62bca7242b9274e8489660c7b5f88de
step-off <nofantasy12@gmail.com>
2019-11-07 09:08:11 +0500

    Add mappings between VehicleResource and Vehicle

--- Mapping/MappingProfile.cs
+++ Mapping/MappingProfile.cs
@@ -1,6 +1,8 @@
+using System.Linq;
 using AutoMapper;
 using vega.Controllers.Resources;
 using vega.Domain.Models;
+using vega.Domain.Models.Vehicle;
 
 namespace vega.Mapping
 {
@@ -8,8 +10,23 @@ namespace vega.Mapping
     {
         public MappingProfile()
         {
+            // Domain to API
             CreateMap<VehicleMake, VehicleMakeResource>();
             CreateMap<VehicleModel, VehicleModelResource>();
+             CreateMap<Vehicle, VehicleResource>()
+             .ForMember(v => v.Contact, opt => opt.MapFrom(resource => new ContactResource { 
+                 Name = resource.ContactName, 
+                 Email = resource.ContactEmail,
+                 Phone = resource.ContactPhone 
+            }))
+            .ForMember(v => v.Features, opt => opt.MapFrom(resource => resource.Features.Select(i => i.FeatureId)));
+
+            // API to Domain
+            CreateMap<VehicleResource, Vehicle>()
+            .ForMember(v => v.ContactName, opt => opt.MapFrom(resource => resource.Contact.Name))
+            .ForMember(v => v.ContactEmail, opt => opt.MapFrom(resource => resource.Contact.Email))
+            .ForMember(v => v.ContactPhone, opt => opt.MapFrom(resource => resource.Contact.Phone))
+            .ForMember(v => v.Features, opt => opt.MapFrom(resource => resource.Features.Select(id => new VehicleFeature {FeatureId = id})));
         }
     }
 }
